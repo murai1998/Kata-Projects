b945ec3dcde10a2731a692550da87852ebbc1f18
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-09-05 17:39:58 -0500

    Add files via upload

---
+++ simplepig.js
@@ -0,0 +1,13 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str) {
+  return str
+    .split(" ")
+    .map(x => x.replace(/(\b)$/, x[0] + "ay"))
+    .map(x => x.replace(/^(\w)/, ""))
+    .join(" ");
+}
