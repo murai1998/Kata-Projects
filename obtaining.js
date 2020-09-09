6abba41b40788aec7dba5a0b71c7229825aeca97
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-09-09 16:51:40 -0500

    Add files via upload

---
+++ obtaining.js
@@ -0,0 +1,31 @@
+// You are given two strings s and t. Both strings have length n and consist of lowercase Latin letters.
+
+// You can successively perform the following move any number of times (possibly, zero):
+
+// swap any two adjacent (neighboring) characters of s (i.e. for any i={0,1,2,…,n−2} you can swap si and si+1).
+// Input:
+// Strings s and t, which consisting of n(1 ≤ n ≤ 1000) lowercase Latin letters.
+
+// Output:
+// The minimum number of moves to transform s to t. If it is impossible to obtain the string t using moves, return -1.
+
+// Examples:
+
+// ('abcdef', 'abdfec') => 4
+// ('abcd', 'accd') => -1
+// ('ab', 'ab') => 0
+// ('ab', 'ba') => 1
+// ('aaa', 'aaa') => 0
+// Note:
+// In the first example the string s changes as follows: "abcdef" → "abdcef" → "abdcfe" → "abdfce" → "abdfec".
+
+function obtain(s, t) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    let index = t.indexOf(s[i]);
+    if (index === -1) return -1;
+    t = t.slice(0, index) + t.slice(index + 1);
+    count += index;
+  }
+  return count;
+}
