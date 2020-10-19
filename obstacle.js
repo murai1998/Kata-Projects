63e8312a948bb6cf71f0c56f5c3a5bf10b28c1f3
Hanna Murai <57275209+murai1998@users.noreply.github.com>
2020-10-18 21:17:33 -0500

    Add files via upload

---
+++ obstacle.js
@@ -0,0 +1,11 @@
+// Obstacles properties: x, y, width, height, image, health
+// Obstacles methods: shoot, move, detectCollision
+class Obstacle {
+    constructor(){
+        // Player properties: x, y, width, height, image, health
+        this.x = 0
+        this.y = 0
+        this.width = 64
+        this.height = 64
+    }
+}
